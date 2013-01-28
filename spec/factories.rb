d0c6959f6a7e85d740834905bba3885e6dd2c11d
YAMANE Toshiaki <yamanetoshi@gmail.com>
2013-01-28 10:38:06 +0900

    Finish user edit, update, index, and destroy actions

--- spec/factories.rb
+++ spec/factories.rb
@@ -6,11 +6,14 @@
 #end
 
 FactoryGirl.define do
-  factory :user do |u|
-    u.name     { "Michael Hartl" }
-    u.email    { "michael@example.com" }
-    u.password { "foobar" }
-    u.password_confirmation { "foobar" }
+  factory :user do
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com"}
+    password "foobar"
+    password_confirmation "foobar"
+
+    factory :admin do
+      admin true
+    end
   end
 end
-
